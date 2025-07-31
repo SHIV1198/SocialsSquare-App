48704f87f9c9c6a80b37b26f9a64404482dc646c
shiva <shiv.shivamanand@gmail.com>
2025-07-31 21:35:45 +0530

    Project 1st added.

---
+++ square/src/main/java/com/socials/square/SocialsSquareApplication.java
@@ -0,0 +1,13 @@
+package com.socials.square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialsSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialsSquareApplication.class, args);
+	}
+//sdasd
+}
